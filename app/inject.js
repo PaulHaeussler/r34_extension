ac3e6472a53d6a29ec37c0b52c5510cb0cf6bb71
Paul <paulhaeussler@gmx.de>
2020-10-07 00:01:45 +0200

    Initial commit

---
+++ app/inject.js
@@ -0,0 +1,72 @@
+window.onload=function(){
+    console.log("page load!");
+}
+
+fetch("http://hentai.bilbosjournal.com/allGroups", {headers: [
+        ['Content-Type', 'application/x-www-form-urlencoded'],
+        ['Content-Type', 'multipart/form-data'],
+        ['Content-Type', 'text/plain'],
+    ]})
+    .then(response => response.text())
+    .then((response) => {
+        console.log(response)
+        var input = document.getElementById("tags");
+        var search = input.value.replace(" ", "");
+
+
+
+
+        var match = false;
+        response.split("=====").forEach(function(item){
+            if(item.replace(" ", "") === "") return;
+            if(search === item) {
+                match = true;
+                console.log(item + " matched!")
+            }
+
+            var tags = document.getElementById("tag-sidebar");
+            const listArray = Array.from(tags.children);
+            listArray.forEach((item2) => {
+                var tmp = item2.innerHTML.split("</a>")
+                if(tmp.length > 1) {
+                    var tmp2 = tmp[0].split(">");
+                    var tmp3 = tmp2[tmp2.length - 1];
+                    if (tmp3.replace(" ", "_") === item.replace(" ", "")){
+                        console.log(tmp3);
+                        item2.className = "";
+                        const arr = Array.from(item2.children);
+                        arr.forEach((item3) => {
+                            item3.style.color = "white";
+                            item3.style.backgroundColor = "#5A00FF";
+                        })
+                    }
+                }
+            });
+
+            var postList = document.getElementById("post-list");
+            if(postList === null) return;
+            var content = postList.getElementsByClassName("content")[0];
+
+                var divSpans = content.getElementsByTagName('div')[0];
+                const divArr = Array.from(divSpans.children);
+                divArr.forEach((item2) => {
+                   var a = item2.getElementsByTagName('a')[0];
+                   var img = a.getElementsByTagName('img')[0];
+
+                   var alt = img.getAttribute('alt');
+
+                   alt.split(" ").forEach((item3) => {
+                       if(item3 === "") return;
+                       if(item3 === item){
+                           img.style = "border:10px outset #5A00FF";
+                       }
+                   });
+                });
+        });
+
+        if(match) {
+            input.style.color = "white";
+            input.style.backgroundColor = "#5A00FF";
+        }
+    })
+    .catch(err => console.log(err))
