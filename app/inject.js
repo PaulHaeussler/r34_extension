4272f3edd8acc7de365bfc0ade6891907c867da6
PaulHaeussler <33423467+PaulHaeussler@users.noreply.github.com>
2022-12-30 15:27:31 +0100

    fixed several issues, only favs are not working yet

--- app/inject.js
+++ app/inject.js
@@ -12,9 +12,11 @@ fetch("http://hentai.bilbosjournal.com/allGroups", {headers: [
     .then((response) => {
         console.log(response)
         var input = document.getElementsByName("tags")[0];
-        var search = input.value.replace(" ", "");
-
-
+        var search = ""
+        if(input !== undefined){
+         search = input.value.replace(" ", "");
+        }
+        var postList = document.getElementById("post-list");
 
 
         var match = false;
@@ -26,53 +28,67 @@ fetch("http://hentai.bilbosjournal.com/allGroups", {headers: [
             }
 
             var tags = document.getElementById("tag-sidebar");
-            const listArray = Array.from(tags.children);
-            listArray.forEach((item2) => {
-                var tmp = item2.innerHTML.split("</a>")
-                if(tmp.length > 1) {
-                    var tmp2 = tmp[0].split(">");
-                    var tmp3 = tmp2[tmp2.length - 1];
-                    if (tmp3.replace(new RegExp(' ', "g"), "_") === item || tmp3 === item){
-                        console.log(tmp3);
-                        item2.className = "";
-                        const arr = Array.from(item2.children);
-                        arr.forEach((item3) => {
-                            item3.style.color = "white";
-                            item3.style.backgroundColor = "#5A00FF";
-                        })
+            if(tags !== null){
+                const listArray = Array.from(tags.children);
+                listArray.forEach((item2) => {
+                    var tmp = item2.innerHTML.split("</a>\n<span class=\"tag-count\">")
+                    if(tmp.length > 1) {
+                        if(postList === null){
+                            var tmp2 = tmp[0].split(">");
+                        } else {
+                            var tmp2 = tmp[0].split(">\n");
+                        }
+
+                        var tmp3 = tmp2[tmp2.length - 1].trim();
+                        if (tmp3.replace(new RegExp(' ', "g"), "_") === item || tmp3 === item || tmp3.replace(' ', '_') === item){
+                            console.log(tmp3);
+                            item2.className = "";
+                            const arr = Array.from(item2.children);
+                            arr.forEach((item3) => {
+                                item3.style.color = "white";
+                                item3.style.backgroundColor = "#5A00FF";
+                            })
+                        }
                     }
-                }
-            });
-
-            var postList = document.getElementById("post-list");
-            if(postList === null) return;
-            var content = postList.getElementsByClassName("content")[0];
-
-                var divSpans = content.getElementsByClassName('image-list')[0];
-                arr = Array.from(divSpans.getElementsByClassName('thumb'))
-                arr.forEach((item2) => {
-                   var a = item2.getElementsByTagName('a')[0];
-                   if (a === undefined) return;
-                   var img = a.getElementsByTagName('img')[0];
-                   if (img === undefined) return;
-
-                   var alt = img.getAttribute('alt');
-                   if(alt === null) {
-                    alt = a.getElementsByTagName('img')[1];
-                   }
-
-
-                   alt.split(" ").forEach((item3) => {
-                       if(item3 === "") return;
-                       if(item3 === item){
-                           if(img.style.border !== ""){
-                                img.style = "border:10px outset #B300FF";
-                           } else {
-                                img.style = "border:10px outset #5A00FF";
-                           }
-                       }
-                   });
                 });
+            }
+
+
+
+            if(postList === null){
+            // favorites
+                console.log("parsing favs")
+            } else {
+            // Normal search result page
+                var content = postList.getElementsByClassName("content")[0];
+
+                    var divSpans = content.getElementsByClassName('image-list')[0];
+                    arr = Array.from(divSpans.getElementsByClassName('thumb'))
+                    arr.forEach((item2) => {
+                       var a = item2.getElementsByTagName('a')[0];
+                       if (a === undefined) return;
+                       var img = a.getElementsByTagName('img')[0];
+                       if (img === undefined) return;
+
+                       var alt = img.getAttribute('alt');
+                       if(alt === null) {
+                        alt = a.getElementsByTagName('img')[1];
+                       }
+
+
+                       alt.split(" ").forEach((item3) => {
+                           if(item3 === "") return;
+                           if(item3 === item){
+                               if(img.style.border !== ""){
+                                    img.style = "border:10px outset #B300FF";
+                               } else {
+                                    img.style = "border:10px outset #5A00FF";
+                               }
+                           }
+                       });
+                    });
+            }
+
         });
 
         if(match) {
